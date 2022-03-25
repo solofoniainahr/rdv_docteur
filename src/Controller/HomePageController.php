527032c61c6a12e5003037c6c86e7ca83c838c80
solofoniaina <solofoniainahr@gmail.com>
2022-03-25 20:09:03 +0300

    Feat:pagination doctor

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use App\Data\SearchDoctorData;
 use App\Form\SearchFormDoctor;
 use App\Repository\UserRepository;
-use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,16 +15,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(Request $request, UserRepository $ur): Response
+    public function index(Request $request, UserRepository $ur, PaginatorInterface $paginator): Response
     {
         $data = new SearchDoctorData;
         $form_search = $this->createForm(SearchFormDoctor::class, $data);
         $form_search->handleRequest($request);
-        $doctors = $ur->findBy(['is_doctor' => 1]);
+        $practiciens = $ur->findBy(['is_doctor' => 1]);
         
         if ($form_search->isSubmitted() && $form_search->isValid()) { 
-            $doctors = $ur->findDoctor($data);
+            $practiciens = $ur->findDoctor($data);
         }
+
+        $doctors = $paginator->paginate(
+            $practiciens, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            1 // Nombre de résultats par page
+        );
         
         return $this->render('home_page/index.html.twig', [
             'form' => $form_search->createView(),
